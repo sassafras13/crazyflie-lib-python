d034e97ef1df5a4dcf63f7c834136d0e2c79b61b
Emma Benjaminson <ebenjami@andrew.cmu.edu>
2019-11-30 17:32:22 -0500

    working min_jerk_test2.py

--- examples/min_jerk_test2.py
+++ examples/min_jerk_test2.py
@@ -14,9 +14,15 @@ from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.position_hl_commander import PositionHlCommander
+from cflib.positioning.motion_commander import MotionCommander
 
 import rospy
 from std_msgs.msg import String
+# Messages
+from geometry_msgs.msg import Pose, Point, Quaternion
+from optitrack.msg import RigidBody, RigidBodyArray
+from optitrack.utils import get_ip_address, read_parameter
+
 
 ###############
 # GLOBAL VARS #
@@ -29,6 +35,7 @@ uri = 'radio://0/80/250K'
 d = 4.0 # [sec] total duration of trajectory
 
 # initial conditions x 
+x0 = 0 
 xdot0 = 0 # [m/s] initial velocity xdot
 xddot0 = 0 # [m/s2] initial accln xddot
 
@@ -53,8 +60,73 @@ zddotf = 0 # [m/s2] final accln zddot
 #############
 # SETUP LOG #
 #############
+# Reference: usermanual.wiki/Document/crazyflieGuide.1291615246/view
+dataLog = errorLog = pos_writer = log_timestamp = log_conf = Logger = None
 
-logging.basicConfig(level=logging.ERROR)
+#########################
+# CLASS:SKELETON LOGGER #
+#########################
+#class SkeletonLogger:
+#    def __init__(self, cf):
+#        self.cf = cf
+#        logging.basicConfig(level=logging.ERROR)
+
+#    def start_logging(self):
+#        global pos_writer
+#        pos_log_file = open(self.directory + self.log_timestamp +'_pos.csv', 'wb') 
+#        pos_writer = csv.writer(pos_log_file)
+#        pos_writer.writerow(['time','x_pos','y_pos','z_pos','yaw'])
+#        log_pos = LogConfig(name='Position',period_in_ms=10)
+
+#       try:
+#            log_pos.add_variable('kalman.stateX', 'float')
+#            log_pos.add_variable('kalman.stateY', 'float')
+#            log_pos.add_variable('kalman.stateZ', 'float') 
+#           log_pos.add_variable('stabilizer.yaw', 'float') # from basicLog.py
+#            self.cf.log.add_config(log_pos)
+
+            # this callback will receive the data
+#            log_pos.data_received_cb.add_callback(print_pos)
+
+            # this callback will be called on errors
+            #log_pos.error_cb.add_callback(log_error)
+
+            # start the logging
+#            log_pos.start()
+
+            # return the yaw
+#            print('Stabilizer: ' + log_pos['stabilizer.yaw'])
+#            return log_pos['stabilizer.yaw']
+
+#        except KeyError as e:
+#            print('Could not start log configuration,''{} not found in TOC'.format(str(e)))
+#        except AttributeError:
+#            print('Could not add Position log config, bad configuration.')
+
+#    def print_pos(timestamp, data, self):
+#
+#        print([timestamp, data['kalman.stateX'], data['kalman.stateY'], data['kalman.stateZ'], data['stabilizer.yaw']])
+
+    #def log_error(self, logconf, msg):
+
+        # call back from the log API when an error occurs
+        #print('Error when logging %s: %s' % (logconf.name, msg))
+        #if self.errorLog == None:
+        #    if not os.path.exists(DIRECTORY):
+        #        os.makedirs(DIRECTORY)
+        #    self.errorLog = open(DIRECTORY + datetime.datetime.now().strftime('Error Log %Y-%m-%d_%H:%M:%S'), 'a')
+        #else:
+        #    self.errorLog.write('Error when logging %s: %s\n' % (logconf.name, msg))
+
+
+####################
+# FN:BEGIN LOGGING #
+####################
+#def begin_logging(cf):
+#    global Logger
+#    Logger = SkeletonLogger(cf)
+#    yaw = Logger.start_logging()
+#    return yaw
 
 ###################
 # FN:COEFFICIENTS #
@@ -91,7 +163,8 @@ def trajectory(coeffs, t):
 ########################
 
 def followTrajectory(tf,dt,adjStartPos,adjTargetPos):
-    
+    print('i am in trajectory')
+
     # pull out position coordinates
     x0 = adjStartPos[0]
     y0 = adjStartPos[1]
@@ -136,37 +209,62 @@ def followTrajectory(tf,dt,adjStartPos,adjTargetPos):
         time.sleep(dt)
      
 
-################
-# FN:GET START #
-################
-def getStart(topic_name):
-    rospy.Subscriber(topic_name, String)
-    # write code here to get the position of the crazyflie
+#####################
+# FN:GET START DATA #
+#####################
+def getStartData(topic_name): 
+    data = rospy.wait_for_message(topic_name, RigidBodyArray)
+    #print(data)
+    #print(data.bodies[0].pose.position.x)
+    x = data.bodies[0].pose.position.x
+    y = data.bodies[0].pose.position.y
+    z = data.bodies[0].pose.position.z
+    return [x,y,z]
 
-    return [x0, y0, z0]
-
-#################
-# FN:GET TARGET #
-#################
-def getTarget(topic_name):
-    rospy.Subscriber(topic_name, String)
-    # write code here to get the position of the landing point
-    
-    return [xf, yf, zf]
-
-###################
-# FN:ADJUST START #
-###################
-def adjustStart(startPos):
-    adjStartPos = startPos - startPos
-    return adjStartPos
+######################
+# FN:GET TARGET DATA #
+######################
+def getTargetData(topic_name): 
+    data = rospy.wait_for_message(topic_name, RigidBodyArray)
+    #print(data)
+    #print(data.bodies[1].pose.position.x)
+    x = data.bodies[1].pose.position.x
+    y = data.bodies[1].pose.position.y
+    z = data.bodies[1].pose.position.z
+    return [x,y,z]
 
 ####################
 # FN:ADJUST TARGET #
 ####################
-def adjustTarget(startPos,targetPos):
-    adjTargetPos = targetPos - startPos
-    return adjTargetPos
+def adjustTarget(targetPos):
+    x = targetPos[0]
+    y = targetPos[1]
+    z = targetPos[2]
+
+    xa = x - x
+    ya = y - y
+    za = z - z
+
+    return [xa,ya,za]
+
+###################
+# FN:ADJUST START #
+###################
+def adjustStart(startPos,targetPos):
+    xs = startPos[0]
+    ys = startPos[1]
+    zs = startPos[2]
+
+    xt = targetPos[0]
+    yt = targetPos[1]
+    zt = targetPos[2]
+
+    xa = xs - xt 
+    ya = ys - yt
+    za = zs - zt
+
+    return [xa,ya,za]
+
 
 ########
 # MAIN #
@@ -188,25 +286,34 @@ if __name__ == '__main__':
             cf.param.set_value('kalman.resetEstimation','0')
             time.sleep(2)
 
+            # begin logging
+            #yaw = begin_logging(cf)
+
             with MotionCommander(scf) as mc:
+                time.sleep(1)
+
                 # take off is done automatically when we open this context
-                
-                # get the yaw from the Optical Flow deck
-                # I think this needs to create a log and get the yaw from the TOC
-                yaw = getYaw()
+                mc.up(1.5)
+
+                time.sleep(4)
+
+                print('completed takeoff')
 
                 # rotate so the Crazyflie is aligned to the Optitrack system
-                mc.turn_left(yaw)
+                #mc.turn_left(yaw)
                 
                 # get the starting point and target point from Optitrack
                 topic_name = '/optitrack/rigid_bodies'
-                startPos = getStart(topic_name)
-                targetPos = getTarget(topic_name)
+                startData = getStartData(topic_name)
+                targetData = getTargetData(topic_name)
 
                 # shift the coordinate system so startPos is the origin
                 # get targetPos relative to startPos
-                adjStartPos = adjustStart(startPos)
-                adjTargetPos = adjustTarget(startPos,targetPos)
+                adjStartPos = adjustStart(startData,targetData)
+                adjTargetPos = adjustTarget(targetData)
+
+                print(adjStartPos)
+                print(adjTargetPos)
 
                 # run the trajectory planner
                 tf = 10.0 # [s] elapsted time
