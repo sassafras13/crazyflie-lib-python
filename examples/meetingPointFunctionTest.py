743ac84b22287147a2eaf151764c6973deb09bcb
Emma Benjaminson <ebenjami@andrew.cmu.edu>
2019-12-10 23:28:59 -0500

    add code for 2d meeting point

--- examples/meetingPointFunctionTest.py
+++ examples/meetingPointFunctionTest.py
@@ -10,7 +10,7 @@ import math
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def meetingPoint(droneVel, ugvVel, dronePos, ugvPos):
+def meetingPoint3D(droneVel, ugvVel, dronePos, ugvPos):
 	
 	# get difference in x,y,z components between 2 vehicles
 	dx = dronePos[0] - ugvPos[0]
@@ -87,14 +87,68 @@ def meetingPoint(droneVel, ugvVel, dronePos, ugvPos):
 	# return desired target points for each vehicle as x,y,z components
 	return [droneX,droneY,ugvX,ugvY]
 
+def meetingPoint2D(droneVel, ugvVel, dronePos, ugvPos):
+	
+	# get difference in x,y,z components between 2 vehicles
+	dx = dronePos[0] - ugvPos[0]
+	dy = dronePos[1] - ugvPos[1]
+	print('dx = ', dx, ' dy = ', dy)
+
+	# calculate azimuth and zenith angles
+	phi = math.atan2(dy,dx)
+	dist = math.sqrt((dx**2) + (dy**2))
+
+	# calculate time required to get to crossing point
+	time = dist / abs(droneVel - ugvVel)
+	print('time = ', time, 'drone vel = ', droneVel, ' ugv vel = ', ugvVel)
+
+	# calculate distance traveled at time of crossing for each vehicle
+	droneDist = abs(droneVel * time) 
+	ugvDist = abs(ugvVel * time)
+	print('droneDist = ', droneDist)
+	print('ugvDist = ', ugvDist)
+
+	# decompose distances into components
+	# drone
+	droneXi = droneDist * math.cos(phi)
+	droneYi = droneDist * math.sin(phi)
+
+	# ugv
+	ugvXi = ugvDist * math.cos(phi)
+	ugvYi = ugvDist * math.sin(phi)
+
+	# add distances to current positions
+	# SUBTRACT distance from DRONE
+	# ADD distance to UGV
+	droneX = dronePos[0] - droneXi
+	droneY = dronePos[1] - droneYi
+	ugvX = ugvPos[0] + ( ugvXi)
+	ugvY = ugvPos[1] + ( ugvYi)
+
+	# plot current and destination points for each vehicle
+	fig = plt.figure()
+	ax1 = fig.add_subplot(111)
+	ax1.scatter(dronePos[0], dronePos[1], c='blue',label='Drone Start Point')
+	ax1.scatter(ugvPos[0], ugvPos[1],c='green',label='Turtlebot Start Point')
+	ax1.scatter(droneX, droneY, c='red',label='Meeting Point')
+	ax1.scatter(ugvX, ugvY, c='red')
+	ax1.set_xlabel('X (m)')
+	ax1.set_ylabel('Y (m)')
+	ax1.set_title('Optimal Meeting Point for Two Vehicles in 3D Space (Drone Faster)')
+	ax1.legend(loc = 'center left') 
+	plt.show()
+	# return desired target points for each vehicle as x,y,z components
+	return [droneX,droneY,ugvX,ugvY]
+
+
 if __name__ == '__main__':
 
-	droneVel = -0.1 
+	droneVel = -0.8 
 	ugvVel = 0.5
 	dronePos = [1.0,1.0,1.0]
 	ugvPos = [0.0,0.0,0.0]
 
 	#print('drone vel = ', droneVel, ' ugvVel = ', ugvVel)
-	point = meetingPoint(droneVel, ugvVel, dronePos, ugvPos)
+	point = meetingPoint2D(droneVel, ugvVel, dronePos, ugvPos)
 
 	print('dx = ', point[0], ' dy = ', point[1], 'ux = ', point[3], ' uy = ', point[4]) 
